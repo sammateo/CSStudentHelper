142c9a195b4535d2f6fec2c85029a51533f91a32
Mateo Sam <sammateo4@gmail.com>
2021-12-17 14:29:48 -0400

    Third attempt at hosting

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 const app = express();
@@ -25,6 +26,10 @@ app.use("/students", studentsRouter);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("../build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
+	});
 }
 
 app.listen(port, () => {
